e338868068b5b132c93c3f74be1f99f8e127bd4e
BlastoisexD <143332182+BlastoisexD@users.noreply.github.com>
2024-04-19 12:29:30 +0545

    Initial commit

---
+++ app/src/main/java/com/example/amishgram/signup.kt
@@ -0,0 +1,13 @@
+package com.example.amishgram
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class signup : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+
+    }
+
+}
